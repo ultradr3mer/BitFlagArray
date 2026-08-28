3f282abe768441c5636bb74982ba73b4b5eeb86d
ultradr3mer <dulz@hotmail.de>
2026-08-28 14:49:22 +0200

    update

--- GenricTable.py
+++ GenricTable.py
@@ -3,43 +3,47 @@ import numpy as np
 import numpy.typing as npt
 
 
-T_cols = TypeVar('T_cols', bound=Any)
+class NpColDef(NamedTuple):
+    name: str
+    type: npt.DTypeLike
 
 
+# dtype-spec input form: [('kind', np.bool), ('abs_min', np.uint64), ...]
+type FieldSpec = list[tuple[str, npt.DTypeLike]]
 
-T_col_ary_item = T_cols # TypeVar('T_col_ary_item', bound=Any)
-type T_cols_ary[T_cols] = npt.NDArray[T_cols]
-T_col_scl_item = T_cols # TypeVar('T_col_scl_item', bound=np.ScalarType)
-type T_cols_item[T_col_scl_item] = T_col_scl_item
 
-type T_cols_ary_or_item[T_cols] = T_cols_ary[T_cols] | T_cols_item[T_cols]
+def to_col_defs(spec: FieldSpec) -> list[NpColDef]:
+    """Convert a raw (name, dtype) field-spec into a list of NpColDef."""
+    return [NpColDef(name, np.dtype(dt)) for name, dt in spec]
 
-tbl_bool = TypeVar('tbl_bool', bound=T_cols_ary_or_item[np.bool])
-tbl_uint64 = TypeVar('tbl_uint64', bound=T_cols_ary_or_item[np.uint64])
-tbl_uint8 = TypeVar('tbl_uint8', bound=T_cols_ary_or_item[np.uint8])
 
-# T_contain = TypeVar('T_contain', bound=npt.NDArray |np.ScalarType)
-type T_ary = npt.NDArray
-type T_cols_of_container[T_contain, T_cols] = T_contain[T_cols]
-# def try1():
+T_base = TypeVar('T_base', bound=Any)
+T_row = TypeVar('T_row', bound=Any)
 
-class TblAndColumn(NamedTuple, Generic[tbl_bool,tbl_uint64,tbl_uint8]):
-    kind: tbl_bool
-    abs_min: tbl_uint64
-    max: tbl_uint64
-    bits: tbl_uint8
 
-class MyTable(TblAndColumn[npt.NDArray[np.bool],npt.NDArray[np.uint64],npt.NDArray[np.uint8]]):
-    pass
-
-class MyRow(TblAndColumn[np.bool,np.uint64,np.uint8]):
+class AnotherTbl(Generic[T_base, T_row]):
     pass
 
 
-test_tbl = MyTable(kind=np.array([True]), abs_min=np.array([1]), max=np.array([1]), bits=np.array([1]))
+def create_table_type(
+    table_name: str,
+    fields: list[NpColDef],
+    ary: np.ndarray | None = None,
+):
+    row_fields: list[tuple[str, type[Any]]] = [(n, np.dtype(t).type) for n, t in fields]
+    row_type = NamedTuple(f"{table_name}Row", row_fields)
+    table_fields: list[tuple[str, type[Any]]] = [
+        (n, npt.NDArray[np.dtype(t)]) for n, t in fields
+    ]
+    table_fields.append(("row_type", row_type))
+    tbl_type = NamedTuple(f"{table_name}Table", table_fields)
+    return tbl_type
 
-print(test_tbl)
 
-test_row = MyRow(kind=True, abs_min=1, max=1, bits=1)
+_test_spec = [('kind', np.bool), ('abs_min', np.uint64), ('max', np.uint64), ('bits', np.uint8)]
+_col_defs = to_col_defs(_test_spec)
+print(_col_defs)
+_test = create_table_type("MyTypes", _col_defs)
+print(_test)
 
-print(test_row)
+_test(kind=np.array((True)))
